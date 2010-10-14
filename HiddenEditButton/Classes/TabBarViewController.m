9b3921fb9883a23c9922ca34e3b42dfbb47345df
Jeff Verkoeyen <jverkoey@gmail.com>
2010-10-14 11:00:12 -0400

    Update the source after testing.
    Add support for Xcode 3.2.5.

--- HiddenEditButton/Classes/TabBarViewController.m
+++ HiddenEditButton/Classes/TabBarViewController.m
@@ -37,6 +37,24 @@
                     nil]];
 
   self.customizableViewControllers = nil;
+
+  // Due to a bug in iOS 4.0, we need to manually hide the edit button when the edit controller
+  // appears.
+  NSString* systemVersion = [[UIDevice currentDevice] systemVersion];
+  if ([systemVersion versionStringCompare:@"4.0"] >= 0
+      && [systemVersion versionStringCompare:@"4.1"] < 0) {
+    self.moreNavigationController.delegate = self;
+  }
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)navigationController: (UINavigationController *)navigationController
+      willShowViewController: (UIViewController *)viewController
+                    animated: (BOOL)animated {
+  UINavigationBar* morenavbar = navigationController.navigationBar;
+  UINavigationItem* morenavitem = morenavbar.topItem;
+  morenavitem.rightBarButtonItem = nil;
 }
 
 
