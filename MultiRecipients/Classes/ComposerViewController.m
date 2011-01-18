8abe581d068523445687afe08b10389b65f6532f
John Wang <john@johntwang.com>
2011-01-17 22:33:58 -1000

    [Sample] Datasource was set to nil instead of the message controller's dataSource.

--- MultiRecipients/Classes/ComposerViewController.m
+++ MultiRecipients/Classes/ComposerViewController.m
@@ -20,20 +20,23 @@
 
 - (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
   if ([textField isKindOfClass:[TTPickerTextField class]]) {
+    NSLog(@"is picker");
       for (int i = 0; i < _fields.count; ++i) {
         id field = [_fieldViews objectAtIndex:i];
         if (field == textField) {
           switch (i) {
             case 0: {
               id ccField = [_fieldViews objectAtIndex:1];
-              [(id)textField setDataSource:[ccField dataSource]];
+              [textField setDataSource:[self dataSource]];
               [ccField setDataSource:nil];
+              NSLog(@"to");
               break;
             }
             case 1: {
               id toField = [_fieldViews objectAtIndex:0];
-              [(id)textField setDataSource:[toField dataSource]];
+              [textField setDataSource:[self dataSource]];
               [toField setDataSource:nil];
+              NSLog(@"cc");
               break;
             }
             default:
